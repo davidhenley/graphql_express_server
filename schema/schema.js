e12f390daac6a2bb135eb03667a3fd178a853af9
davidhenley <henleydr@gmail.com>
2017-03-03 09:45:59 -0600

    RootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,5 @@
 const GraphQL = require('graphql');
+const _ = require('lodash');
 
 const {
   GraphQLObjectType,
@@ -6,11 +7,30 @@ const {
   GraphQLInt
 } = GraphQL;
 
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+  { id: '12', firstName: 'John', age: 27 }
+];
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
   }
 });

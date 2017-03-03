99939855749f63bab48386f31b671e07f203c6a3
davidhenley <henleydr@gmail.com>
2017-03-03 00:40:09 -0600

    Using expressGraphQL on /graphql

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
 
 app.listen(4000, () => {
   console.log('Magic happens on port 4000');
